9004795ff324d06ae3b11dce9d9dcea4be946b35
katafuchix <katafuchix@gmail.com>
2025-06-27 20:11:17 +0900

    Initial Commit

---
+++ OverlayToastExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OverlayToastExample
+//
+//  Created by cano on 2025/06/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OverlayToastExample/OverlayToastExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverlayToastExampleApp.swift
+//  OverlayToastExample
+//
+//  Created by cano on 2025/06/27.
+//
+
+import SwiftUI
+
+@main
+struct OverlayToastExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
